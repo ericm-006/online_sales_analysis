c304346e21c5be0cdefb209bf8b8d2f09e5ee740
Milos <ericm4586@gmail.com>
2026-04-17 13:42:04 +0200

    Update main before cart merge

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-p1 = Product("Laptop", 1000, 5)
-p2 = Product("Telefon", 500, 10)
-p3 = Product("Miš", 20, 50)
+p1 = Product("Laptop PRO", 1200, 3)
+p2 = Product("Telefon X", 700, 8)
+p3 = Product("Gaming miš", 40, 30)
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
 
-manager.display_products()
-print("Ukupna vrednost:", manager.total_value())
+# manager.display_products()
+# print("Ukupna vrednost:", manager.total_value())
+
+
